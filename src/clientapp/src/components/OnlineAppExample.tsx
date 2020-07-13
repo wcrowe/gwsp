273f15e734af95507582a694ee3689cd29d739ad
wcrowe <crowe.will@gmail.com>
2020-07-12 21:16:49 -0400

    Updates

--- src/clientapp/src/components/OnlineAppExample.tsx
+++ src/clientapp/src/components/OnlineAppExample.tsx
@@ -9,24 +9,24 @@ type FormData = {
   Email: string;
   // DOB: Date;
   FirstName: string;
-//   MiddleName?: string;
-//   LastName: string;
-//   Address: string;
-//   AddressExt?: string;
-//   City: string;
-//   ZipCode: string;
-//   PhoneNumber: string;
-//   PhoneNumberExt: string;
-//   AlternatePhone: string;
+  MiddleName?: string;
+  LastName: string;
+  Address: string;
+  AddressExt?: string;
+  //   City: string;
+  //   ZipCode: string;
+  //   PhoneNumber: string;
+  //   PhoneNumberExt: string;
+  //   AlternatePhone: string;
 };
 
 const schema = yup.object().shape({
   FirstName: yup.string().required("First name is required"),
-//   LastName: yup.string().required("Last name is required"),
-//   Address: yup.string().required("Address is required"),
-//   City: yup.string().required("City is required"),
-//   ZipCode: yup.string().required("Zip is required"),
-//   PhoneNumber: yup.string().required("Phone is required"),
+  LastName: yup.string().required("Last name is required"),
+  Address: yup.string().required("Address is required"),
+  //   City: yup.string().required("City is required"),
+  //   ZipCode: yup.string().required("Zip is required"),
+  //   PhoneNumber: yup.string().required("Phone is required"),
   Email: yup
     .string()
     .required("Email address is required")
@@ -46,8 +46,8 @@ type RBRef = string & ((ref: Element | null) => void);
 
 //  <Input ref={register} name="LastName" />
 const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => (
-    <input ref={ref} {...props} />
-  ));
+  <input ref={ref} {...props} />
+));
 
 const OnlineAppExample = () => {
   const { register, control, handleSubmit, errors } = useForm<FormData>({
@@ -56,7 +56,7 @@ const OnlineAppExample = () => {
 
   const onSubmit: SubmitHandler<FormData> = (data) => {
     console.log(JSON.stringify(data));
-  }
+  };
 
   return (
     <Container>
@@ -69,7 +69,7 @@ const OnlineAppExample = () => {
               placeholder="Email"
               name="Email"
               ref={register}
-              maxLength={10}
+              maxLength={100}
               isInvalid={!!errors.Email}
             ></Form.Control>
             <Form.Control.Feedback type="invalid">
@@ -78,7 +78,7 @@ const OnlineAppExample = () => {
           </Form.Group>
         </Form.Row>
         <Form.Row>
-          <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Group as={Col} controlId="formGridFirstName">
             <Form.Label>First Name</Form.Label>
             <Form.Control
               type="text"
@@ -86,6 +86,7 @@ const OnlineAppExample = () => {
               name="FirstName"
               isInvalid={!!errors.FirstName}
               ref={register}
+              maxLength={100}
             ></Form.Control>
             <Form.Control.Feedback type="invalid">
               {errors.FirstName?.message}
@@ -93,7 +94,63 @@ const OnlineAppExample = () => {
           </Form.Group>
         </Form.Row>
         <Form.Row>
-          <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Group as={Col} controlId="formGridMiddleName">
+            <Form.Label>Middle Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Middle Name"
+              name="MiddleName"
+              ref={register}
+              maxLength={100}
+            ></Form.Control>
+          </Form.Group>
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} controlId="formGridLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Last Name"
+              name="LastName"
+              isInvalid={!!errors.LastName}
+              ref={register}
+              maxLength={100}
+            ></Form.Control>
+            <Form.Control.Feedback type="invalid">
+              {errors.LastName?.message}
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} controlId="formGridAddress">
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Address"
+              name="Address"
+              isInvalid={!!errors.Address}
+              ref={register}
+              maxLength={200}
+            ></Form.Control>
+            <Form.Control.Feedback type="invalid">
+              {errors.Address?.message}
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} controlId="formGridAddressExt">
+            <Form.Label>Address 2</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Address 2"
+              name="AddressExt"
+              ref={register}
+              maxLength={200}
+            ></Form.Control>
+          </Form.Group>
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} controlId="formGridSubmit">
             <Button type="submit" variant="primary" size="lg" active block>
               Sign in
             </Button>
